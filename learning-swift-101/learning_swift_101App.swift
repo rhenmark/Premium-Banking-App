3e7c9dcff63ce6c29edbefcb51fce955ed50c2a9
Rhen Mark Callado <rhenmarkc@gmail.com>
2023-02-12 10:58:31 +0800

    Initial Commit

---
+++ learning-swift-101/learning_swift_101App.swift
@@ -0,0 +1,17 @@
+//
+//  learning_swift_101App.swift
+//  learning-swift-101
+//
+//  Created by Rhen Mark on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct learning_swift_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
